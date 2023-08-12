17082f1fca631f2c481bfa9e8ef3685befd273b0
rafaelHsieh <rafaelhsieh0813@gmail.com>
2023-08-12 23:08:50 +0800

    blog with 50% completeness: search function works

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Canvas } from "react-three-fiber";
-import { Box, Text } from "@react-three/drei";
 
 const App: React.FC = () => {
-  return <div className="App">Welcome vmks forum</div>;
+  return (
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog:id" element={<Blog />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
